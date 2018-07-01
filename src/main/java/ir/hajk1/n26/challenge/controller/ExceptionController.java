4169c782482aa88fa53401962469a312ec928b03
Kayvan Hajhassantehrani <k1.tehrani@gmail.com>
2018-07-01 11:55:22 +0430

    Exception message is logged

--- src/main/java/ir/hajk1/n26/challenge/controller/ExceptionController.java
+++ src/main/java/ir/hajk1/n26/challenge/controller/ExceptionController.java
@@ -1,7 +1,6 @@
 package ir.hajk1.n26.challenge.controller;
 
 
-import ir.hajk1.n26.challenge.service.TransactionServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -13,13 +12,15 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class ExceptionController {
+
   private static final Logger logger = LoggerFactory.getLogger(ExceptionController.class);
 
   @ResponseStatus(HttpStatus.NO_CONTENT)
   @ExceptionHandler(Exception.class)
-  public ResponseEntity<Void> handleException() {
-    if(logger.isErrorEnabled())
-      logger.error("Invalid Request received");
+  public ResponseEntity<Void> handleException(Exception e) {
+    if (logger.isErrorEnabled()) {
+      logger.error("Invalid Request received:" + e.getMessage());
+    }
     return new ResponseEntity<>(HttpStatus.NO_CONTENT);
   }
 }
