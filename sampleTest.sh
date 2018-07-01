9afbccd25884830d28b1b53e4e19ec19696e7f72
Kayvan Hajhassantehrani <k1.tehrani@gmail.com>
2018-07-01 11:56:13 +0430

    current millis is now exactly before future!

--- sampleTest.sh
+++ sampleTest.sh
@@ -2,6 +2,6 @@
 COUNTER=0
 while [  $COUNTER -lt 500 ]; do
 	amnt=$RANDOM
-   curl -X POST --header 'Content-Type: application/json' http://127.0.0.1:8080/transactions -d "{\"amount\": $amnt, \"timestamp\": $(date +%s999)}"
+   curl -X POST --header 'Content-Type: application/json' http://127.0.0.1:8080/transactions -d "{\"amount\": $amnt, \"timestamp\": $(date +%s000)}"
    let COUNTER=COUNTER+1
 done
