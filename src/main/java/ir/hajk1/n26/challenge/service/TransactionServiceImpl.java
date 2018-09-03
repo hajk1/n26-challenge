315aeeae90e45f787944d8f67995261f9a2905ed
k1.tehrani@gmail.com <BlackHeart59>
2018-09-04 00:50:52 +0430

    some more enhancements to make the cache auto evicting

--- src/main/java/ir/hajk1/n26/challenge/service/TransactionServiceImpl.java
+++ src/main/java/ir/hajk1/n26/challenge/service/TransactionServiceImpl.java
@@ -19,61 +19,60 @@ import java.time.ZoneId;
 @Service
 public class TransactionServiceImpl implements TransactionService {
 
-  private static final Logger logger = LoggerFactory.getLogger(TransactionServiceImpl.class);
+    private static final Logger logger = LoggerFactory.getLogger(TransactionServiceImpl.class);
 
-  /**
-   * This 60 length array holds each second transactions
-   */
-  private TransactionAmountListPerSecond[] transactionAmountListPerSeconds = new TransactionAmountListPerSecond[60];
+    /**
+     * This 60 length array holds each second transactions
+     */
+    private TransactionAmountListPerSecond[] transactionAmountListPerSeconds = new TransactionAmountListPerSecond[60];
 
-  public TransactionServiceImpl() {
-    for (int i = 0; i < transactionAmountListPerSeconds.length; i++) {
-      transactionAmountListPerSeconds[i] = new TransactionAmountListPerSecond();
+    public TransactionServiceImpl() {
+        for (int i = 0; i < transactionAmountListPerSeconds.length; i++) {
+            transactionAmountListPerSeconds[i] = new TransactionAmountListPerSecond();
+        }
     }
-  }
 
-  public TransactionAmountListPerSecond[] getTransactionAmountListPerSeconds() {
-    return transactionAmountListPerSeconds;
-  }
-
-  private void addTransaction(Transaction transaction) {
-    byte second = extractSecondPortion(transaction.getTimestamp());
-      transactionAmountListPerSeconds[second].addNewTransaction(transaction.getAmount());
-//    synchronized (transactionAmountListPerSeconds[second]) {
-//      transactionAmountListPerSeconds[second].getAmountList().add(transaction.getAmount());
-//    }
-  }
-
-  /**
-   * Extract Second portion of timestamp
-   *
-   * @return (0 - 59)
-   */
-  private byte extractSecondPortion(long timestamp) {
-    LocalDateTime transactionDate =
-        LocalDateTime.ofInstant(Instant.ofEpochMilli(timestamp), ZoneId.systemDefault());
-    return (byte) transactionDate.getSecond();
-  }
-
-  @Override
-  public void persist(@Valid Transaction transaction) {
-    if (logger.isInfoEnabled()) {
-      logger.info("new transaction has been received: " + transaction);
+    public TransactionAmountListPerSecond[] getTransactionAmountListPerSeconds() {
+        return transactionAmountListPerSeconds;
     }
-    addTransaction(transaction);
-  }
 
-  /**
-   * Runs Every second and resets transaction amounts for that special second
-   */
-  @Scheduled(cron = "0/1 * * * * *")
-  public void expireCacheCron() {
-    byte currentSecond = (byte) LocalDateTime.now().getSecond();
-    synchronized (transactionAmountListPerSeconds[currentSecond]) {
-      transactionAmountListPerSeconds[currentSecond].reset();
+    private void addTransaction(Transaction transaction) {
+        byte second = extractSecondPortion(transaction.getTimestamp());
+        synchronized (transactionAmountListPerSeconds[second]) {
+            transactionAmountListPerSeconds[second].addNewTransaction(transaction.getAmount());
+        }
     }
-    if (logger.isDebugEnabled()) {
-      logger.debug("resetting Amounts for expired second index:" + currentSecond);
+
+    /**
+     * Extract Second portion of timestamp
+     *
+     * @return (0 - 59)
+     */
+    private byte extractSecondPortion(long timestamp) {
+        LocalDateTime transactionDate =
+                LocalDateTime.ofInstant(Instant.ofEpochMilli(timestamp), ZoneId.systemDefault());
+        return (byte) transactionDate.getSecond();
     }
-  }
+
+    @Override
+    public void persist(@Valid Transaction transaction) {
+        if (logger.isInfoEnabled()) {
+            logger.info("new transaction has been received: " + transaction);
+        }
+        addTransaction(transaction);
+    }
+
+    /**
+     * Runs Every second and resets transaction amounts for that special second
+     */
+    @Scheduled(cron = "0/1 * * * * *")
+    public void expireCacheCron() {
+        byte currentSecond = (byte) LocalDateTime.now().getSecond();
+        synchronized (transactionAmountListPerSeconds[currentSecond]) {
+            transactionAmountListPerSeconds[currentSecond].reset();
+        }
+        if (logger.isDebugEnabled()) {
+            logger.debug("resetting Amounts for expired second index:" + currentSecond);
+        }
+    }
 }
